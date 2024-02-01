ae52cf4de8afa522a0d260d53cd6f062c9801daf
Dmitriy Basenko <d.basenko.it@gmail.com>
2024-02-01 13:36:05 +0500

    fix: Добавить глобальные переменные для сборки

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,12 +2,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/dsbasko/yandex-go-shortener/internal/app"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
+func printGreeting() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n\n", buildCommit)
+}
+
 func main() {
+	printGreeting()
+
 	if err := app.Run(); err != nil {
 		log.Panic(err.Error())
 	}
