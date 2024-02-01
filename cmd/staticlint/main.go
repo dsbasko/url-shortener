feac645bc4849c52bdf42b5d2f613497597d22af
Dmitriy Basenko <d.basenko.it@gmail.com>
2024-02-01 13:32:41 +0500

    fix: Архитектурный рефакторинг

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"golang.org/x/tools/go/analysis"
-	"golang.org/x/tools/go/analysis/multichecker"
-
-	"github.com/dsbasko/yandex-go-shortener/internal/linter"
-)
+import "github.com/dsbasko/yandex-go-shortener/pkg/linter"
 
 func main() {
-	var analyzers []*analysis.Analyzer
-
-	analyzers = append(analyzers, linter.GetXPassesAnalyzers()...)
-	analyzers = append(analyzers, linter.GetStaticCheckAnalyzers()...)
-	analyzers = append(analyzers, linter.GetStyleCheckAnalyzers()...)
-	analyzers = append(analyzers, linter.GetExternalAnalyzers()...)
-	analyzers = append(analyzers, linter.NoExitInMainAnalyzer)
-
-	multichecker.Main(analyzers...)
+	linter.Run()
 }
